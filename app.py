20cbdf42e472fe81fff25d5001bfad901a2088ec
Nicole Ramirez <80730603+nicovelarde@users.noreply.github.com>
2023-07-31 13:46:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,27 @@ data_training_array = scaler.fit_transform(data_training)
 
 
 #Load my model
-model = load_model('keras_model.h5')
+#model = load_model('keras_model.h5')
+from keras.layers import Dense, Dropout, LSTM
+from keras.models import Sequential
+
+model = Sequential()
+model.add(LSTM(units =50, activation = 'relu', return_sequences=True,input_shape=(x_train.shape[1],1)))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units =60, activation = 'relu', return_sequences=True))
+model.add(Dropout(0.3))
+
+model.add(LSTM(units =80, activation = 'relu', return_sequences=True))
+model.add(Dropout(0.4))
+
+model.add(LSTM(units =120, activation = 'relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss = 'mean_squared_error')
+model.fit(x_train, y_train, epochs=50)
 
 #Testing Part
 
